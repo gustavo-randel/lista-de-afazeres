7317daba5b0c763f46e0d08ba2b3ad3093b32eed
Gustavo Randel <137479113+gustavo-randel@users.noreply.github.com>
2026-01-18 18:57:19 -0300

    inicio da estrutura de formulario

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,14 @@ function App() {
   
   return (
     <>
-    
+    <form className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input type="text" id="item" />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+    <h1 className="header">Todo List</h1>
     </>
   )
 }
